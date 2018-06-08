25658006c40443f91cf8718262adc61e95263f13
philippe <philippe@SIMOCCAUCH26.lan>
2018-06-08 14:35:00 +0200

    Annuaire5

--- abc.php
+++ abc.php
@@ -3,10 +3,14 @@
 
 <head>
 
+<h1>THE ANNUAIRE</h1>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="style.css">
-
-
+	
 	<title>THE ANNUAIRE</title>
 
 </head>
