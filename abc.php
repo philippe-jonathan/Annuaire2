7cdce53935bcc5841b39aed89b6802949640b49e
philippe <philippe@SIMOCCAUCH26.home>
2018-06-10 13:10:38 +0200

    Annuaire11

--- abc.php
+++ abc.php
@@ -90,4 +90,28 @@ $del = $bdd->query('DELETE FROM contacts WHERE id =1')
 
 
 </body>
+
+	<footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">THE ANNUAIRE</h5>
+                <p class="grey-text text-lighten-4">Trouvez plus que de simple coordonnées.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Liens</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Contact</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2018 Simplon.co
+            <a class="grey-text text-lighten-4 right" href="#!">Infos</a>
+            </div>
+          </div>
+        </footer>
+
 </html>
